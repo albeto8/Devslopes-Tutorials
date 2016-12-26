ab98509ed525189cf3bffcb827ecfd1cbc603fd0
Mario Alberto Barragán Espinosa <albeto8@gmail.com>
2016-12-25 20:35:57 -0600

    Tableviews & implementing a protocol

---
+++ DevslopesTutorials/Video.m
@@ -0,0 +1,13 @@
+//
+//  Video.m
+//  DevslopesTutorials
+//
+//  Created by Mario Alberto Barragán Espinosa on 25/12/16.
+//  Copyright © 2016 mario. All rights reserved.
+//
+
+#import "Video.h"
+
+@implementation Video
+
+@end
--- DevslopesTutorials/ViewController.m
+++ DevslopesTutorials/ViewController.m
@@ -8,9 +8,12 @@
 
 #import "ViewController.h"
 #import "HTTPService.h"
+#import "Video.h"
+#import "CustomCell.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic,strong) NSArray *videoList;
 @end
 
 @implementation ViewController
@@ -18,20 +21,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [[HTTPService instance] getTutorials:^(NSDictionary * _Nullable dataDict, NSString * _Nullable errMessage) {
-        if (dataDict) {
-            NSLog(@"Dictionary: %@", dataDict.debugDescription);
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    self.videoList = [[NSArray alloc]init];
+    
+    [[HTTPService instance] getTutorials:^(NSArray * _Nullable dataArray, NSString * _Nullable errMessage) {
+        if (dataArray) {
+            //NSLog(@"Dictionary: %@", dataArray.debugDescription);
+            NSMutableArray *arr = [[NSMutableArray alloc]init];
+            
+            for (NSDictionary *dict in dataArray) {
+                Video *video = [[Video alloc]init];
+                video.videoTitle = [dict objectForKey:@"title"];
+                video.videoDescription = [dict objectForKey:@"description"];
+                video.thumbnailUrl = [dict objectForKey:@"thumbnail"];
+                video.videoIframe = [dict objectForKey:@"iframe"];
+                
+                [arr addObject:video];
+            }
+            
+            self.videoList = arr;
+            [self updateTableData];
+            
         } else if (errMessage) {
             //Display alert to user
         }
     }];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) updateTableData {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.tableView reloadData];
+    });
 }
 
+-(void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    CustomCell *cell = (CustomCell*)[tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    if (!cell) {
+        cell = [[CustomCell alloc]init];
+    }
+    return cell;
+}
+
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.videoList.count;
+}
+
+-(NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
 
 @end
